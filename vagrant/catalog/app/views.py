dbc7dcdf839ec7cf500ef3e735e388011b8f3d78
Scott Harman <scott@harman.tv>
2015-12-18 16:19:53 +1100

    changing layouts and trimming the fat.

--- vagrant/catalog/app/views.py
+++ vagrant/catalog/app/views.py
@@ -5,6 +5,26 @@ from flask import jsonify, render_template
 
 
 @app.route('/')
+@app.route('/home/')
+def homepage():
+    """Returns the last 5 products for each category"""
+    """Should have two panels - LHS lists categories, and initial RHS view lists most recent products across all categories plus price and category
+    category and/or product are clickable, to return either the category page or the product page.
+    Users can edit, create, or delete only the items that they created
+    """
+    #productItems = access.getProducts()
+    #for product in productItems:
+    #    product.category = access.getCategory(product.category_id)
+    productItems = []
+    categories = access.getCategories()
+    for category in categories:
+        productItems += access.getProductCountCategory(category.category_id, 5)
+    for product in productItems:
+        product.category_name = access.getCategory(product.category_id)
+    return render_template('producthome.html', categories=categories,
+        products=productItems)
+
+
 @app.route('/products/')
 def products():
     """Returns the last 5 products for each category"""
